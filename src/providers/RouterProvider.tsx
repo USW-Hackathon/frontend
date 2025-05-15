b3ab9f8effd0ebab3146b4fd8ad76311d8197f3b
parksomii <somi020605@gmail.com>
2025-05-16 03:21:19 +0900

    feat: 공지사항 페이지 추가
    /notice

--- src/providers/RouterProvider.tsx
+++ src/providers/RouterProvider.tsx
@@ -4,12 +4,14 @@ import LoginPage from '@/pages/LoginPage';
 import SidePage from '@/pages/SidePage';
 import HomePage from '../pages/HomePage';
 import Location from '../pages/Location';
+import NoticePage from '../pages/NoticePage';
 
 const RouterProvider = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/notice" element={<NoticePage />} />
         <Route path="/side" element={<SidePage />} />
         <Route path="/api" element={<ApiTestPage />} />
         <Route path="/login" element={<LoginPage />} />
